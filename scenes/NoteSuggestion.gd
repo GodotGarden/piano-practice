0f5563219b98c409113ad8326ab63a28e2840ccf
Brylie Christopher Oxley <brylie@protonmail.com>
2024-01-30 00:22:26 +0200

    Initial working note suggestion with delay

--- scenes/NoteSuggestion.gd
+++ scenes/NoteSuggestion.gd
@@ -13,7 +13,7 @@ var current_note = ""
 func select_and_display_note():
 	var random_index = randi() % NOTE_NAMES.size()
 	current_note = NOTE_NAMES[random_index]
-	# Assuming you have a Label node in your scene to display the note
+	note_label.modulate = Color.WHITE
 	note_label.text = current_note
 
 
@@ -22,13 +22,22 @@ func _ready():
 	MIDIHandler.connect("midi_note_on", self._on_midi_note_on)
 	randomize()
 	select_and_display_note()
+	var delay_timer = $DelayTimer
+	delay_timer.wait_time = 1.0  # Set the desired delay time
+	delay_timer.one_shot = true
+	delay_timer.autostart = false
 
 func _on_midi_note_on(note_info: MIDIHandler.MIDINoteInfo):
 	if note_info.pitch_class == current_note:
 		emit_signal("note_correct")
+		# Visual feedback for correct note
+		note_label.modulate = Color.GREEN
 		print("Correct note played!")
+		$DelayTimer.start()
+		await $DelayTimer.timeout
+		select_and_display_note()
 	else:
 		emit_signal("note_incorrect")
+		# Visual feedback for incorrect note
+		note_label.modulate = Color.RED
 		print("Incorrect note played!")
-	# Optionally, select and display a new note
-	select_and_display_note()
